8617c236ed934d106f2eae97d8ff1a3b47910027
Laura Asher <laura.asher@jhuapl.edu>
2023-01-26 11:04:04 -0500

    Progress

--- pairwise_similarity.py
+++ pairwise_similarity.py
@@ -1,11 +1,18 @@
 import sys
+from math import pi
 import numpy as np
+import pandas as pd
 import seaborn as sns
 import matplotlib.pylab as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 sys.path.append("..")
 from documents import document_list
+
+
+# from bokeh.models import BasicTicker, ColorBar, LinearColorMapper, PrintfTickFormatter
+# from bokeh.plotting import figure, show
+
 
 corpus = document_list
 vect = TfidfVectorizer(min_df=1, stop_words="english")
@@ -21,18 +28,65 @@ print(sim_arr)
 # plt.show()
 
 print(np.amax(sim_arr))
-mask = np.zeros_like(sim_arr)
-mask[np.triu_indices_from(mask)] = True
-with sns.axes_style("ticks"):
-    ax = sns.heatmap(
-        sim_arr,
-        mask=mask,
-        vmin=np.amin(sim_arr),
-        vmax=np.amax(sim_arr),
-        square=True,
-        cmap="YlGnBu",
-    )
-    plt.show()
+# mask = np.zeros_like(sim_arr)
+# mask[np.triu_indices_from(mask)] = True
+# with sns.axes_style("ticks"):
+#     ax = sns.heatmap(
+#         sim_arr,
+#         mask=mask,
+#         vmin=np.amin(sim_arr),
+#         vmax=np.amax(sim_arr),
+#         square=True,
+#         cmap="magma_r",
+#     )
+#     plt.show()
 
 # Could organize by similarity... thematically
 # Or offer chronolgical and similarity sorting options
+
+df = pd.DataFrame(sim_arr)
+mask = pd.DataFrame(np.triu(np.ones(df.shape)).astype(bool))
+df[mask] = 0
+import plotly.express as px
+fig = px.imshow(df, color_continuous_scale='hot')
+fig.show()
+# print(df)
+
+# years = list(data.index)
+# months = list(data.columns)
+
+# # reshape to 1D array or rates with a month and year for each row.
+# df = pd.DataFrame(data.stack(), columns=['rate']).reset_index()
+
+# # this is the colormap from the original NYTimes plot
+# colors = ["#75968f", "#a5bab7", "#c9d9d3", "#e2e2e2", "#dfccce", "#ddb7b1", "#cc7878", "#933b41", "#550b1d"]
+# mapper = LinearColorMapper(palette=colors, low=np.amin(sim_arr), high=np.amax(sim_arr))
+
+# TOOLS = "hover,save,pan,box_zoom,reset,wheel_zoom"
+
+# p = figure(title="",
+#        #     x_range=years, y_range=list(reversed(months)),
+#            x_axis_location="above", width=900, height=400,
+#            tools=TOOLS, toolbar_location='below',
+#        #     tooltips=[('date', '@Month @Year'), ('rate', '@rate%')]
+#            )
+
+# p.grid.grid_line_color = None
+# p.axis.axis_line_color = None
+# p.axis.major_tick_line_color = None
+# p.axis.major_label_text_font_size = "7px"
+# p.axis.major_label_standoff = 0
+# p.xaxis.major_label_orientation = pi / 3
+
+# p.rect(x="Year", y="Month", width=1, height=1,
+#        source=df,
+#        fill_color={'field': 'rate', 'transform': mapper},
+#        line_color=None)
+
+# color_bar = ColorBar(color_mapper=mapper, major_label_text_font_size="7px",
+#                      ticker=BasicTicker(desired_num_ticks=len(colors)),
+#                      formatter=PrintfTickFormatter(format="%d%%"),
+#                      label_standoff=6, border_line_color=None)
+# p.add_layout(color_bar, 'right')
+
+# show(p)
